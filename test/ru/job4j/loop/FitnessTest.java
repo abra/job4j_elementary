f89cec93d82dfda56a0abd7ac5f6b8cd4d0d635a
Aydar Khabibullin <aydar.kh@gmail.com>
2020-09-26 15:19:07 +0500

    5.4. Протеиновая диета [#365117]

--- test/ru/job4j/loop/FitnessTest.java
+++ test/ru/job4j/loop/FitnessTest.java
@@ -20,6 +20,12 @@ public class FitnessTest {
     }
 
     @Test
+    public void whenIvanEqualNik() {
+        int month = Fitness.calc(90, 90);
+        assertThat(month, is(1));
+    }
+
+    @Test
     public void whenIvanLessByFewNik() {
         int month = Fitness.calc(50, 90);
         assertThat(month, is(2));
