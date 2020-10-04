efde014d7617e20fae09d84358132755bbed3aef
Aydar Khabibullin <aydar.kh@gmail.com>
2020-10-04 12:49:07 +0500

    5.7. Ипотека [#365119]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int numberOfYears(double amount, double salary, double percent) {
+        int years = 0;
+
+        while (amount > 0) {
+            amount = amount * ((percent + 100)/100) - salary;
+            years++;
+        }
+
+        return years;
+    }
+}
