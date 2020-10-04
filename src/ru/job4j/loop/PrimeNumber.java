2d80c44720e71bc11801c58c594c88832a2c28b4
Aydar Khabibullin <aydar.kh@gmail.com>
2020-10-04 11:57:03 +0500

    5.6. Простые числа [#365118]

---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public static int countPrimeNumbers(int number) {
+        int count = 0;
+
+        for (int i = number; i > 0; i--) {
+            if (CheckPrimeNumber.check(i)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
